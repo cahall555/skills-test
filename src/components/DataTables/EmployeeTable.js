f3113505f6a2dc52639cb507eb1935778c032abc
wonderwarrior <cahall555@gmail.com>
2021-11-28 21:03:37 -0700

    update table pagination issue #3.

--- src/components/DataTables/EmployeeTable.js
+++ src/components/DataTables/EmployeeTable.js
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from 'react';
-import { styled } from '@material-ui/core/styles'
+import PropTypes from 'prop-types';
+import { styled, useTheme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import { listEmployees } from '../../graphql/queries';
 import awsExports from "../../aws-exports";
@@ -28,11 +37,75 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
 
 const EmployeeTable =() => {
 
   const [Employees, setEmployees] = useState([])
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  
 
    useEffect(() => {
     fetchEmployees()
@@ -45,6 +118,19 @@ const EmployeeTable =() => {
       setEmployees(Employees)
     } catch (err) { console.log('error fetching Employees') }
   }
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - Employees.length) : 0;
+
+    const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+    };
+
   return (
     <React.Fragment>
     <TableContainer component={Paper}>
@@ -57,7 +143,10 @@ const EmployeeTable =() => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {Employees.map((Employee) => (
+          {(rowsPerPage > 0
+            ? Employees.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : Employees
+          ).map((Employee) => (
             <StyledTableRow
               key={Employee.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -70,7 +159,33 @@ const EmployeeTable =() => {
   
             </StyledTableRow>
           ))}
+
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={Employees.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
     </React.Fragment>
