1e0d8d4fbd5f720ad6decf53254f78625f50cb3b
wonderwarrior <cahall555@gmail.com>
2021-12-12 11:59:47 -0700

    wip update skills table to remove delete issue #10

--- src/components/DataTables/SkillsTable.js
+++ src/components/DataTables/SkillsTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { styled } from '@material-ui/core/styles'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,17 +8,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/EditOutlined";
-import { gql, useMutation, useQuery } from '@apollo/client';
-import { deleteSkill, updateSkill,deleteEmployeeSkills} from '../../graphql/mutations';
-import { getEmployee } from 'graphql/queries';
 
-const DELETE_SKILL = gql(deleteSkill);
-const UPDATE_SKILL = gql (updateSkill);
-const DELETE_EMPLOYEE_SKILLS =gql(deleteEmployeeSkills);
-const GET_EMPLOYEE = gql(getEmployee);
+
+
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     backgroundColor: theme.palette.primary.main,
@@ -38,17 +30,6 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 
 const SkillsTable =({data}) => {
-  const [deleteSkill] = useMutation(DELETE_SKILL);
-  // const [deleteEmployeeSkill] = useMutation(DELETE_EMPLOYEE_SKILLS);
-  // const { loading, error, employeeData } =  useQuery(GET_EMPLOYEE, { variables: { id } });
-  // if (loading) return 'Loading...';
-  // if (error) return `Error! ${error.message}`;
-
-  // const handleDelete = () => {
-  //   deleteEmployeeSkill({variables:{input: {id: employeeData}}})
-  //   deleteSkill({variables:{input: {id: Skill.id}}})
-  // };
-
 
   return (
     <React.Fragment>
@@ -59,7 +40,6 @@ const SkillsTable =({data}) => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-             <StyledTableCell align="center"></StyledTableCell>
             <StyledTableCell align="center">ID</StyledTableCell>
             <StyledTableCell align="left">Skill</StyledTableCell>
           </TableRow>
@@ -70,14 +50,6 @@ const SkillsTable =({data}) => {
               key={Skill.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell>
-               <IconButton
-                    aria-label="delete"
-                    onClick={()=>{deleteSkill({variables:{input: {id: Skill.id}}})}}
-                  >
-                  <DeleteIcon />
-                </IconButton>
-              </TableCell>
               <StyledTableCell component="th" scope="row" align="center">
                 {Skill.id}
               </StyledTableCell>
